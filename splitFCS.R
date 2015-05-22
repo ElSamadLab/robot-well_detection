4a0ab2dea313f85120a7ee4a9151e908cbef6071
Kieran Mace <kieran.mace@gmail.com>
2015-05-22 15:10:01 -0700

    modified code to track wells and sort fcs file

--- splitFCS.R
+++ splitFCS.R
@@ -1,5 +1,5 @@
-#file.name <- "one_well.fcs"
-file.name <- "multiColor.fcs"
+file.name <- "one_well.fcs"
+#file.name <- "multiColor.fcs"
 
 split.fcs = function(counts, smooth, reasonable.time.min, reasonable.time.max, squash, plot=F, title='default title'){
     counts[counts<=squash]=0
@@ -27,22 +27,27 @@ library(quantmod)
 library(caTools)
 library(signal)
 
-fcs <- read.fcs(file.name)#FCS(file.name), transformation=FALSE)
-times = exprs(fcs)[,"Time"]
+fcs <- exprs(read.fcs(file.name))#FCS(file.name), transformation=FALSE)
+times = fcs[,"Time"]
 s = 100; # Convert to seconds
-counts = hist(times,breaks=seq(from=min(times)-s,to=max(times)+s,by=s))$counts # per second
+limits = seq(from=min(times)-s,to=max(times)+s,by=s)
+time.buckets = hist(times,breaks=limits)
+counts = time.buckets$counts # per second
 
 # define a plate region
 pdf('test.pdf')
-
+ 
 plate.splits = split.fcs(counts, 20, 1000, 2000, squash=15, plot=T, title='test')
 
-well.splits.starts = list()
-well.splits.ends = list()
+plate = list()
 for( i in 1:length(plate.splits$start)){
     well.splits = split.fcs(counts[plate.splits$start[i]:plate.splits$end[i]], 1, 5, 15, 10, plot=T, title=paste('plate',toString(i)))
-    well.splits.starts[[i]]=well.splits$start
-    well.splits.ends[[i]]=well.splits$end
+    plate[[i]] = list()
+    for (j in 1:length(well.splits$start)){
+        plate[[i]][[j]]=fcs[which(times>limits[well.splits$start[j]]&times<limits[well.splits$start[j+1]]),]
+    }
 }
+
+
 
 dev.off()
