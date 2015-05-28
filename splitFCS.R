9d6a6da537b2f19204e2eb1aa4d5d6afbf3951bf
Kieran Mace <kieran.mace@gmail.com>
2015-05-28 15:14:00 -0700

    added the best parameters so far to fit alain kieran and patrick datasets

--- splitFCS.R
+++ splitFCS.R
@@ -1,12 +1,13 @@
 file.name <- "one_well.fcs"
 #file.name <- "multiColor.fcs"
+#file.name <- "patrick.fcs"
 
 split.fcs = function(counts, smooth, reasonable.time.min, reasonable.time.max, squash, plot=F, title='default title'){
     counts[counts<=squash]=0
     counts.smooth = runmean(counts,smooth,align='center')
     possible.breakpoints = which(counts.smooth==0)
     distance = diff(possible.breakpoints)
-    reasonable.distances = which(distance>reasonable.time.min&distance<reasonable.time.max)
+    reasonable.distances = which(distance>=reasonable.time.min&distance<=reasonable.time.max)
     actual.breakpoints.start = possible.breakpoints[reasonable.distances]
     actual.breakpoints.end =   possible.breakpoints[reasonable.distances + 1] 
 
@@ -22,12 +23,12 @@ split.fcs = function(counts, smooth, reasonable.time.min, reasonable.time.max, s
     output$end = actual.breakpoints.end
     return(output)
 }
-library(prada)
+library(flowCore)
 library(quantmod)
 library(caTools)
 library(signal)
 
-fcs <- exprs(read.fcs(file.name))#FCS(file.name), transformation=FALSE)
+fcs <- exprs(read.FCS(file.name, transformation=FALSE, truncate_max_range = FALSE))
 times = fcs[,"Time"]
 s = 100; # Convert to seconds
 limits = seq(from=min(times)-s,to=max(times)+s,by=s)
@@ -41,7 +42,7 @@ plate.splits = split.fcs(counts, 20, 1000, 2000, squash=15, plot=T, title='test'
 
 plate = list()
 for( i in 1:length(plate.splits$start)){
-    well.splits = split.fcs(counts[plate.splits$start[i]:plate.splits$end[i]], 1, 5, 15, 10, plot=T, title=paste('plate',toString(i)))
+    well.splits = split.fcs(counts[plate.splits$start[i]:plate.splits$end[i]], smooth=1, reasonable.time.min=5, reasonable.time.max=14, 10, plot=T, title=paste('plate',toString(i)))
     plate[[i]] = list()
     for (j in 1:length(well.splits$start)){
         plate[[i]][[j]]=fcs[which(times>limits[well.splits$start[j]]&times<limits[well.splits$start[j+1]]),]
